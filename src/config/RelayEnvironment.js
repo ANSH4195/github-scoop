dc874dfad1ddcf9f1fdaafdc2d9d72e0f6420ec4
Ansh <shri.ansh4563@gmail.com>
2021-05-17 16:01:21 +0530

    Netlify Config

--- src/config/RelayEnvironment.js
+++ src/config/RelayEnvironment.js
@@ -1,13 +1,10 @@
 import { Environment, Network, RecordSource, Store } from 'relay-runtime'
 import fetchData from './fetchData'
 
-// Relay passes a "params" object with the query name and text. So we define a helper function
-// to call our fetchData utility with params.text.
 async function fetchRelay(params, variables) {
   return fetchData(params.text, variables)
 }
 
-// Export a singleton instance of Relay Environment configured with our network function:
 export default new Environment({
   network: Network.create(fetchRelay),
   store: new Store(new RecordSource())
